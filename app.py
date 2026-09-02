81eef7fbbfd43163851523d5e0bcef8b3ef63c81
Juan Robledo <juanchisrobledo@gmail.com>
2026-09-02 16:09:33 -0500

    fix: changed app.py logic

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import pymysql
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    usuarios = []
     try:
         conn = pymysql.connect(
             host='db',
             user=os.getenv('MYSQL_USER'),
-            password=os.getenv('MYSQL_ROOT_PASSWORD'),
-            database='users'
+            password=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DATABASE')
         )
         conn.close()
         db_status = "Connected to the database successfully."
     except Exception as e:
-        db_status = f"Error while connecting to the database: {e}"
+        app.logger.error(f"DB connection error: {e}")
+        db_status = "Error connecting to the database."
 
-    return render_template('index.html', db_status=db_status)
+    return render_template('index.html', db_status=db_status, usuarios=usuarios)
 
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
-    app.run(host='0.0.0.0', port=5050, debug=debug_mode) # nosec B104
+    app.run(host='0.0.0.0', port=5050, debug=debug_mode)  # nosec B104
